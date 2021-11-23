532db43846637516a2f11d5af5ddba9c207bc1cf
hejixuan <hejixuan@hejixuandeMacBook-Air.local>
2021-11-23 22:26:39 +0800

    feat: pingpong without lock

--- user/pingpong.c
+++ user/pingpong.c
@@ -4,43 +4,47 @@
 
 int 
 main(int argc, char *argv[]){
-    char msg = '0'; // signal
+    char buf1, buf2 = 0;
     int l_to_r[2], r_to_l[2];   // pipe
+    int counter = 0;
     pipe(l_to_r);
     pipe(r_to_l);
     if(fork() == 0){    // left-child
         // close the default stdin/stdout
-        close(0);
-        close(1);
-        dup(l_to_r[1]); // write
-        dup(r_to_l[0]); // read
-        close(l_to_r[0]);
-        close(l_to_r[1]);
-        close(r_to_l[0]);
-        close(r_to_l[1]);
-        write(1, &msg, 1);
-        printf("%d: ping", getpid());
-        read(0, &msg, 1);
+        // dup(l_to_r[1]); // write
+        // dup(r_to_l[0]); // read
+        // close(l_to_r[0]);
+        // close(l_to_r[1]);
+        // close(r_to_l[0]);
+        // close(r_to_l[1]);
+        while(1){
+            if(counter > 10) break;
+            if(write(l_to_r[1], &buf1, 1) == 1){
+                printf("%d: ping\n", getpid());
+            }
+            if(read(r_to_l[0], &buf2, 1) == 1){
+                printf("%d: pong\n", getpid());
+                counter++;
+            }
+        }
+        // write(l_to_r[1], &msg, 1);
+        // printf("%d: ping\n", getpid());
+        // read(r_to_l[0], &msg, 1);
+        exit(0);
     }
-    if(fork() == 0){    // right-child
-        // close the default stdin/stdout
-        close(0);
-        close(1);
-        dup(r_to_l[1]); // write
-        dup(l_to_r[0]); // read
-        close(l_to_r[0]);
-        close(l_to_r[1]);
-        close(r_to_l[0]);
-        close(r_to_l[1]);
-        read(0, &msg, 1);
-        printf("%d: pong", getpid());
-        write(1, &msg, 1);
+    else{
+        // wait(0);
+        while(1){
+            if(counter > 10) break;
+            if(read(l_to_r[0], &buf2, 1) == 1){
+                printf("%d: pong\n", getpid());
+                counter++;
+            }
+            if(write(r_to_l[1], &buf1, 1) == 1){
+                printf("%d: ping\n", getpid());
+            }
+        }
     }
-    close(l_to_r[0]);
-    close(l_to_r[1]);
-    close(r_to_l[0]);
-    close(r_to_l[1]);
-    wait(0);
-    wait(0);
+    // pingpang();
     exit(0);
 }
